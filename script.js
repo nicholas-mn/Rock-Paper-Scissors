3c6e3a366741c87cc3f08dade79bebf557fe8cb5
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2024-11-03 20:14:02 +0100

    feat: getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hello world.");
+
+function getComputerChoice() {
+    
+    let roll = Math.floor(Math.random() * 100)
+
+    if (roll <= 33) {
+        return "rock"
+    } else if (roll <= 66) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+
+}
